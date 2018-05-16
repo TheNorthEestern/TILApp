a8a67aba6fa04f2814dd3417f572da3ea4684120
Kacy James <inquen@gmail.com>
2018-05-15 23:40:38 -0400

    Added testing infra

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,11 +21,22 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   /// Register the configured SQLite database to the database config.
   let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
   let username = Environment.get("DATABASE_USER") ?? "vapor"
-  let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+  let databaseName: String
+  let databasePort: Int
+  
+  if (env == .testing) {
+    databaseName = "vapor-test"
+    databasePort = 5433
+  } else {
+    databaseName = Environment.get("DATABASE_DB") ?? "Vapor"
+    databasePort = 5432
+  }
+  
   let password = Environment.get("DATABASE_PASSWORD") ?? "password"
   
   let databaseConfig = PostgreSQLDatabaseConfig(
     hostname: hostname,
+    port: databasePort,
     username: username,
     database: databaseName,
     password: password)
