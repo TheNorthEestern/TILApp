5b56b3079d0c3e5c9de25f8260be45a0250694ef
Kacy James <inquen@gmail.com>
2018-05-20 21:18:30 -0400

    Removed some cruft from routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,10 +3,6 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-  // Basic "Hello, world!" example
-//  router.get { req in
-//    return "Hello, world!"
-//  }
   
   let acronymsController = AcronymsController()
   let usersController = UsersController()
@@ -17,4 +13,5 @@ public func routes(_ router: Router) throws {
   try router.register(collection: usersController)
   try router.register(collection: categoriesController)
   try router.register(collection: websiteController)
+
 }
