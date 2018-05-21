b5a74bd531d872bb6b89845eb4a3db912aa4a46e
Kacy James <inquen@gmail.com>
2018-05-20 21:04:27 -0400

    Added acronym template logic

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -1,12 +1,23 @@
 import Vapor
 import Leaf
 
+struct IndexContext: Encodable {
+  let title: String
+  let acronyms: [Acronym]?
+}
+
 struct WebsiteController: RouteCollection {
   func boot(router: Router) throws {
     router.get(use: indexHandler)
   }
   
   func indexHandler(_ req: Request) throws -> Future<View> {
-    return try req.view().render("index")
+    return Acronym.query(on: req)
+                  .all()
+      .flatMap(to: View.self) { acronyms in
+        let acronymsData = acronyms.isEmpty ? nil : acronyms
+        let context = IndexContext(title: "Homepage", acronyms: acronymsData)
+        return try req.view().render("index", context)
+    }
   }
 }
