1af0a09b36b9d25a714f51b2481a02ccb98c779e
Kacy James <inquen@gmail.com>
2018-06-13 22:40:59 -0400

    Made minor configuration changes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -36,7 +36,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     }
   } else {
     databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    databasePort = 5432
+    databasePort = 1234
   }
   
   let password = Environment.get("DATABASE_PASSWORD") ?? "password"
