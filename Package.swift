32f0b8bdb2ed5d59e63d6054b67c0805af0020b4
Kacy James <inquen@gmail.com>
2018-06-25 19:20:06 -0400

    Made updates to dependencies

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "TILApp",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.4"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.4"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
     ],
